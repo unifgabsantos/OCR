7795b73c2359e94d4f04d32b083c90cc37ae6db3
Gabriel Lopes Parreira dos Santos <gabriel.lopessb@gmail.com>
2025-03-05 15:11:14 -0300

    Update main.py
    Criei a função de enviar a imagem

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ load_dotenv() # Carregas variaveis de ambiente
 api_key = getenv("ChatGPT_API_Key") # Pega a chave da API do ChatGPT
 
 assistant_id = "asst_iN92X2yDZccGoKvWbGpQyGG3" # ID do assistente
+
+client = OpenAI(api_key=api_key) # Cria o cliente
+
+def upload_image(image_path:str)->str:
+    """
+    Recebe o caminho de uma imagem e a envia para o OpenAI e retorna o ID da imagem.
+    Args:
+        image_path (str): O caminho da imagem.
+    Returns:
+        str: O ID da imagem.
+    """
+    with open(image_path,"rb") as image:
+        response = client.files.create(file=image,purpose="vision")
+        return response["id"]
+    
